0236a011390f9b87314f0fece2a73cd23f8acfba
RaffyJO <rafiteguh6@gmail.com>
2023-11-26 19:42:19 +0700

    add: add page
    1. add search page in web and mobile
    2. add detail page in wen and mobile

--- lib/detailPage.dart
+++ lib/detailPage.dart
@@ -9,6 +9,34 @@ class DetailPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (BuildContext context, BoxConstraints constraints) {
+        if (constraints.maxWidth > 800) {
+          return DetailWebPage(
+            masakan: masakan,
+          );
+        } else {
+          return DetailPageMobile(
+            masakan: masakan,
+          );
+        }
+      },
+    );
+  }
+}
+
+class DetailPageMobile extends StatefulWidget {
+  final Masakan masakan;
+
+  const DetailPageMobile({super.key, required this.masakan});
+
+  @override
+  State<DetailPageMobile> createState() => _DetailPageMobileState();
+}
+
+class _DetailPageMobileState extends State<DetailPageMobile> {
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
         body: SafeArea(
       child: SingleChildScrollView(
@@ -20,7 +48,7 @@ class DetailPage extends StatelessWidget {
                   height: 270,
                   width: MediaQuery.of(context).size.width,
                   child: Image.network(
-                    masakan.imageUrl,
+                    widget.masakan.imageUrl,
                     fit: BoxFit.cover,
                   ),
                 ),
@@ -56,12 +84,12 @@ class DetailPage extends StatelessWidget {
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
                       Text(
-                        masakan.name,
+                        widget.masakan.name,
                         style: const TextStyle(
                             fontSize: 20, fontWeight: FontWeight.w500),
                       ),
                       FavoriteButton(
-                        masakan: masakan,
+                        masakan: widget.masakan,
                       )
                     ],
                   ),
@@ -78,7 +106,7 @@ class DetailPage extends StatelessWidget {
                             width: 2,
                           ),
                           Text(
-                            '${masakan.time} min',
+                            '${widget.masakan.time} min',
                             style: const TextStyle(fontSize: 15),
                           ),
                         ],
@@ -97,7 +125,7 @@ class DetailPage extends StatelessWidget {
                             width: 2,
                           ),
                           Text(
-                            '${masakan.calories} cal',
+                            '${widget.masakan.calories} cal',
                             style: const TextStyle(fontSize: 15),
                           ),
                         ],
@@ -107,23 +135,27 @@ class DetailPage extends StatelessWidget {
                 ],
               ),
             ),
-            Container(
-              margin: const EdgeInsets.only(top: 15, left: 15, right: 15),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const Text(
-                    "Deskripsi",
-                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  Text(
-                    masakan.description,
-                    style: const TextStyle(fontSize: 15),
-                  ),
-                ],
+            Align(
+              alignment: Alignment.topLeft,
+              child: Container(
+                margin: const EdgeInsets.only(top: 15, left: 15, right: 15),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      "Deskripsi",
+                      style:
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Text(
+                      widget.masakan.description,
+                      style: const TextStyle(fontSize: 15),
+                    ),
+                  ],
+                ),
               ),
             ),
             Container(
@@ -139,13 +171,13 @@ class DetailPage extends StatelessWidget {
                     height: 10,
                   ),
                   ListView.builder(
-                      itemCount: masakan.ingredients.length,
+                      itemCount: widget.masakan.ingredients.length,
                       shrinkWrap: true,
                       physics: const NeverScrollableScrollPhysics(),
                       itemBuilder: (context, index) {
                         int count = index + 1;
                         return Text(
-                          "$count. ${masakan.ingredients[index]}",
+                          "$count. ${widget.masakan.ingredients[index]}",
                           style: const TextStyle(fontSize: 15),
                         );
                       })
@@ -165,13 +197,13 @@ class DetailPage extends StatelessWidget {
                     height: 10,
                   ),
                   ListView.builder(
-                      itemCount: masakan.step.length,
+                      itemCount: widget.masakan.step.length,
                       shrinkWrap: true,
                       physics: const NeverScrollableScrollPhysics(),
                       itemBuilder: (context, index) {
                         int count = index + 1;
                         return Text(
-                          "$count. ${masakan.step[index]}",
+                          "$count. ${widget.masakan.step[index]}",
                           style: const TextStyle(fontSize: 15),
                         );
                       })
@@ -185,6 +217,224 @@ class DetailPage extends StatelessWidget {
         ),
       ),
     ));
+  }
+}
+
+class DetailWebPage extends StatefulWidget {
+  final Masakan masakan;
+
+  const DetailWebPage({Key? key, required this.masakan}) : super(key: key);
+
+  @override
+  _DetailWebPageState createState() => _DetailWebPageState();
+}
+
+class _DetailWebPageState extends State<DetailWebPage> {
+  final _scrollController = ScrollController();
+
+  @override
+  Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            vertical: 16,
+            horizontal: 64,
+          ),
+          child: Center(
+            child: SizedBox(
+              width: screenWidth <= 1200 ? 800 : 1200,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SafeArea(
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: CircleAvatar(
+                            backgroundColor: Colors.white,
+                            child: IconButton(
+                              icon: Icon(
+                                Icons.arrow_back,
+                                color: Colors.grey[800],
+                              ),
+                              onPressed: () {
+                                Navigator.pushReplacement(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => const HomePage()),
+                                );
+                              },
+                            ),
+                          ),
+                        ),
+                      ),
+                      const Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: Text(
+                          'Resep Masakan',
+                          style: TextStyle(
+                            fontSize: 32,
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                  const SizedBox(height: 32),
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Expanded(
+                        child: Column(
+                          children: [
+                            ClipRRect(
+                              borderRadius: BorderRadius.circular(10),
+                              child: Image.network(widget.masakan.imageUrl),
+                            ),
+                            const SizedBox(height: 16),
+                          ],
+                        ),
+                      ),
+                      const SizedBox(width: 32),
+                      Expanded(
+                        child: Card(
+                          child: Container(
+                            padding: const EdgeInsets.all(16),
+                            child: Column(
+                              mainAxisSize: MainAxisSize.max,
+                              children: <Widget>[
+                                Text(
+                                  widget.masakan.name,
+                                  textAlign: TextAlign.center,
+                                  style: const TextStyle(
+                                    fontSize: 30.0,
+                                  ),
+                                ),
+                                Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Row(
+                                      children: <Widget>[
+                                        const Icon(Icons.timer_outlined),
+                                        const SizedBox(width: 8.0),
+                                        Text(
+                                          "${widget.masakan.time} min",
+                                        ),
+                                      ],
+                                    ),
+                                    FavoriteButton(
+                                      masakan: widget.masakan,
+                                    ),
+                                  ],
+                                ),
+                                Row(
+                                  children: <Widget>[
+                                    const Icon(
+                                      Icons.local_fire_department_outlined,
+                                    ),
+                                    const SizedBox(width: 8.0),
+                                    Text(
+                                      "${widget.masakan.calories} cal",
+                                    ),
+                                  ],
+                                ),
+                                const SizedBox(height: 16.0),
+                                const Align(
+                                  alignment: Alignment.topLeft,
+                                  child: Text(
+                                    "Deskripsi",
+                                    style: TextStyle(
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.w500),
+                                  ),
+                                ),
+                                Container(
+                                  padding: const EdgeInsets.only(
+                                      bottom: 16.0, top: 10),
+                                  child: Text(
+                                    widget.masakan.description,
+                                    textAlign: TextAlign.justify,
+                                    style: const TextStyle(
+                                      fontSize: 15.0,
+                                    ),
+                                  ),
+                                ),
+                                const Align(
+                                  alignment: Alignment.topLeft,
+                                  child: Text(
+                                    "Alat dan Bahan",
+                                    style: TextStyle(
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.w500),
+                                  ),
+                                ),
+                                Container(
+                                    padding: const EdgeInsets.only(
+                                        bottom: 16.0, top: 10),
+                                    child: ListView.builder(
+                                        itemCount:
+                                            widget.masakan.ingredients.length,
+                                        shrinkWrap: true,
+                                        physics:
+                                            const NeverScrollableScrollPhysics(),
+                                        itemBuilder: (context, index) {
+                                          int count = index + 1;
+                                          return Text(
+                                            "$count. ${widget.masakan.ingredients[index]}",
+                                            style:
+                                                const TextStyle(fontSize: 15),
+                                          );
+                                        })),
+                                const Align(
+                                  alignment: Alignment.topLeft,
+                                  child: Text(
+                                    "Tahap Pembuatan",
+                                    style: TextStyle(
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.w500),
+                                  ),
+                                ),
+                                Container(
+                                    padding: const EdgeInsets.only(
+                                        bottom: 16.0, top: 10),
+                                    child: ListView.builder(
+                                        itemCount: widget.masakan.step.length,
+                                        shrinkWrap: true,
+                                        physics:
+                                            const NeverScrollableScrollPhysics(),
+                                        itemBuilder: (context, index) {
+                                          int count = index + 1;
+                                          return Text(
+                                            "$count. ${widget.masakan.step[index]}",
+                                            style:
+                                                const TextStyle(fontSize: 15),
+                                          );
+                                        })),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
   }
 }
 
--- lib/searchPage.dart
+++ lib/searchPage.dart
@@ -14,24 +14,56 @@ class _SearchPageState extends State<SearchPage> {
 
   @override
   Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (BuildContext context, BoxConstraints constraints) {
+        return Scaffold(
+          appBar: AppBar(
+            elevation: 1,
+            backgroundColor: Colors.white,
+            title: const Text(
+              'Resep Masakan',
+              style: TextStyle(color: Colors.black, fontSize: 17),
+            ),
+            centerTitle: true,
+            leading: IconButton(
+              icon: const Icon(Icons.arrow_back),
+              color: Colors.black, // Icon "back"
+              onPressed: () {
+                // Fungsi yang dipanggil saat tombol "back" ditekan
+                Navigator.of(context).pop();
+              },
+            ),
+          ),
+          body: LayoutBuilder(
+            builder: (BuildContext context, BoxConstraints constraints) {
+              if (constraints.maxWidth <= 930) {
+                return const SearchPageMobile();
+              } else if (constraints.maxWidth <= 1400) {
+                return const SearchPageWeb(gridCount: 2);
+              } else {
+                return const SearchPageWeb(gridCount: 3);
+              }
+            },
+          ),
+        );
+      },
+    );
+  }
+}
+
+class SearchPageMobile extends StatefulWidget {
+  const SearchPageMobile({super.key});
+
+  @override
+  State<SearchPageMobile> createState() => _SearchPageMobileState();
+}
+
+class _SearchPageMobileState extends State<SearchPageMobile> {
+  String searchText = '';
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        elevation: 1,
-        backgroundColor: Colors.white,
-        title: const Text(
-          "Kumpulan Resep",
-          style: TextStyle(color: Colors.black, fontSize: 17),
-        ),
-        centerTitle: true,
-        leading: IconButton(
-          icon: const Icon(Icons.arrow_back),
-          color: Colors.black, // Icon "back"
-          onPressed: () {
-            // Fungsi yang dipanggil saat tombol "back" ditekan
-            Navigator.of(context).pop();
-          },
-        ),
-      ),
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsetsDirectional.fromSTEB(15, 0, 15, 0),
@@ -73,6 +105,9 @@ class _SearchPageState extends State<SearchPage> {
                                       ),
                                     ),
                                   ),
+                                  const SizedBox(
+                                    width: 3,
+                                  ),
                                   Expanded(
                                     flex: 2,
                                     child: Padding(
@@ -102,6 +137,136 @@ class _SearchPageState extends State<SearchPage> {
                         }
                         return null;
                       }),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  List<Masakan> _searchRecipes(String searchText) {
+    return resepMasakan.where((masakan) {
+      return masakan.name.toLowerCase().contains(searchText.toLowerCase());
+    }).toList();
+  }
+
+  Widget searchBox() {
+    return Container(
+      margin: const EdgeInsets.fromLTRB(10, 15, 10, 20),
+      padding: const EdgeInsets.symmetric(horizontal: 5),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(10),
+        border: Border.all(color: Colors.grey),
+      ),
+      child: TextField(
+        onChanged: (value) {
+          setState(() {
+            searchText = value;
+          });
+        },
+        textAlignVertical: TextAlignVertical.center,
+        decoration: const InputDecoration(
+          contentPadding: EdgeInsets.all(0),
+          prefixIcon: Icon(
+            Icons.search,
+            color: Colors.grey,
+            size: 20,
+          ),
+          border: InputBorder.none,
+          hintText: 'Cari resep di sini....',
+          hintStyle: TextStyle(
+            color: Colors.grey,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class SearchPageWeb extends StatefulWidget {
+  final int gridCount;
+
+  const SearchPageWeb({Key? key, required this.gridCount}) : super(key: key);
+
+  @override
+  State<SearchPageWeb> createState() => _SearchPageWebState();
+}
+
+class _SearchPageWebState extends State<SearchPageWeb> {
+  String searchText = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsetsDirectional.fromSTEB(15, 0, 15, 0),
+          child: Container(
+            child: Column(
+              children: [
+                searchBox(),
+                Expanded(
+                  child: GridView.count(
+                    crossAxisCount: widget.gridCount,
+                    crossAxisSpacing: 15,
+                    mainAxisSpacing: 15,
+                    childAspectRatio: 2.5,
+                    children: _searchRecipes(searchText).map((masakan) {
+                      return InkWell(
+                        onTap: () {
+                          Navigator.push(context,
+                              MaterialPageRoute(builder: (context) {
+                            return DetailPage(
+                              masakan: masakan,
+                            );
+                          }));
+                        },
+                        child: Card(
+                          child: Row(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Expanded(
+                                flex: 1,
+                                child: Image.network(
+                                  masakan.imageUrl,
+                                  fit: BoxFit.cover,
+                                  height: MediaQuery.of(context).size.height,
+                                ),
+                              ),
+                              const SizedBox(
+                                width: 3,
+                              ),
+                              Expanded(
+                                flex: 2,
+                                child: Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: <Widget>[
+                                      Text(
+                                        masakan.name,
+                                        style: const TextStyle(
+                                            fontSize: 16.0,
+                                            fontWeight: FontWeight.w500),
+                                      ),
+                                      const SizedBox(
+                                        height: 8,
+                                      ),
+                                      Text(masakan.description),
+                                    ],
+                                  ),
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                      );
+                    }).toList(),
+                  ),
                 )
               ],
             ),
